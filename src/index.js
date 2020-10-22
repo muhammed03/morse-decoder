6574dd8e2402f39554c58438612dcb8b2a48c52b
abdulhakim <hakim@abdulhakims-MacBook-Pro.local>
2020-10-23 00:23:24 +0600

    feat: updated

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let result = '';
+    let letter = '';
+    for (let i = 0; i < expr.length; i += 10){
+        arr.push(expr.slice(i, i+10));
+    }
+
+    for (let i = 0; i< arr.length; i++){
+        if (arr[i]=="**********"){
+            result +=" "; 
+        } else {
+            for(let j = 0; j < arr[i].length; j+=2){
+                if(arr[i].substr(j, 2) == '11'){
+                    letter += '-';
+                }
+                else if(arr[i].substr(j, 2) == '10'){
+                    letter += '.';
+                } 
+            }
+            result += MORSE_TABLE[letter];
+            letter = '';
+            
+        }
+    }
+    return result; 
 }
 
 module.exports = {
